025dbc5e81adaf98d5d03fc5f29388340133b33b
christopher <z5373462@ad.unsw.edu.au>
2025-02-23 18:05:15 +1100

    added tailwind, and search feature

--- patient-app/src/app/components/patient-list/patient-list.component.ts
+++ patient-app/src/app/components/patient-list/patient-list.component.ts
@@ -3,14 +3,16 @@ import { Patient, PatientService } from '../../services/patient.service';
 import { RouterModule } from '@angular/router';
 import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
-  imports: [CommonModule, MatTableModule, RouterModule],
+  imports: [CommonModule, MatTableModule, RouterModule, FormsModule], //, FormsModule ??/
   selector: 'app-patient-list',
   templateUrl: './patient-list.component.html',
   styleUrls: ['./patient-list.component.scss'],
 })
 export class PatientListComponent implements OnInit {
   patients: Patient[] = [];
+  searchTerm: string = '';
   // Pagination settings
   page = 0;
   size = 10;
@@ -26,15 +28,28 @@ export class PatientListComponent implements OnInit {
       .getPatients(this.page, this.size)
       .subscribe((response) => {
         this.patients = response.content;
-        console.log(response)
+        console.log(response);
         console.log(this);
       });
   }
 
-  // Add methods for delete, edit, search etc.
   deletePatient(id: string): void {
     this.patientService.deletePatient(id).subscribe(() => {
       this.loadPatients();
     });
+  }
+
+  searchPatients(): void {
+    console.log("serachinggg")
+    if (this.searchTerm.trim()) {
+      this.patientService
+        .searchPatients(this.searchTerm, this.page, this.size)
+        .subscribe((response) => {
+          this.patients = response.content;
+        });
+    } else {
+      // If no search term, reload the full patient list.
+      this.loadPatients();
+    }
   }
 }
